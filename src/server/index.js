1126db53add7d891cf17fb3f178876fbe5664adf
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-05-01 23:15:48 +0600

    write index response check test code
    write enough code to pass test

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,16 @@
-console.log('It works!');
+const koa = require('koa');
+const app = new koa();
+const PORT = 3001;
+
+app.use(async (ctx) => {
+  ctx.body = {
+    status: 'success',
+    message: 'hello world'
+  };
+});
+
+const server = app.listen(PORT, () => {
+  console.log('app is runnin on port ', PORT);
+});
+
+module.exports = server;
