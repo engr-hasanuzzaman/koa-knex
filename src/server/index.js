ae8714442fe97b96bab6a3f7e8671d5f939e9c2c
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-05-12 22:06:05 +0600

    - install and initialize koa-passport and koa-session for user authentication

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,29 @@
 const koa = require('koa');
 const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
-const app = new koa();
+const passport = require('koa-passport');
+const session = require('koa-session');
+
 const indexRoutes = require('./routes/index');
 const movieRoutes = require('./routes/movies');
+
+const app = new koa();
 const PORT = process.env.PORT || 3001;
 
+// session
+app.keys = ['super-secret-key'];
+app.use(session(app));
+
+// body parser & logger
 app.use(bodyParser());
 app.use(logger());
+
+// authenticat
+require('./auth');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routing
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
 
