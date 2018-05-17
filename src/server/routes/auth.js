0b9807489d9817e937da59585299ef33c15c4361
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-05-17 12:32:21 +0600

    - add login mechanism to project

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -17,8 +17,10 @@ router.get(BASE_PATH, async (ctx) => {
 
 router.post(BASE_PATH, async (ctx) => {
   const user = await queries.addUser(ctx.request.body);
+  console.log('--------- new created user is', user);
   return passport.authenticate('local', (err, user, info, status) => {
     if(user){
+      console.log('--------- with in login user is', user);
       ctx.login(user);
       ctx.redirect('/auth/status');
     }else{
@@ -35,6 +37,38 @@ router.get('/auth/status', async (ctx) => {
   } else {
     ctx.redirect('/auth/login');
   }
+});
+
+router.get('/auth/logout', async (ctx) => {
+  if (ctx.isAuthenticated()) {
+    ctx.logout();
+    ctx.redirect('/auth/login');
+  } else {
+    ctx.body = { success: false };
+    ctx.throw(401);
+  }
+});
+
+router.get('/auth/login', (ctx) => {
+  if(!ctx.isAuthenticated()){
+    ctx.type = 'html';
+    ctx.body = fs.createReadStream('./src/server/views/login.html');
+  }else{
+    ctx.redirect('/auth/status');
+  }
+});
+
+router.post('/auth/login', async (ctx) => {
+  return passport.authenticate('local', (err, user, info, status) => {
+    if (user) {
+      console.log('--- with in login with user', user);
+      ctx.login(user);
+      ctx.redirect('/auth/status');
+    } else {
+      ctx.status = 400;
+      ctx.body = { status: 'error' };
+    }
+  })(ctx);
 });
 
 module.exports = router;
