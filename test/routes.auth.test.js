a2c97e5be52ac652906e269b271b21f1bf96f480
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-05-19 15:15:40 +0600

    write test code for login mechanism

--- test/routes.auth.test.js
+++ test/routes.auth.test.js
@@ -51,4 +51,40 @@ describe('routes: auth', () =>{
       });
     });
   }); // end of post /auth/register 
+
+  describe('GET /auth/login', () => {
+    it('should show user login form', (done) => {
+      chai.request(server)
+      .get('/auth/login')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res.status).eql(200);
+        expect(res.type).eql('text/html');
+        expect(res.text).contain('<h1>Login</h1>');
+        expect(res.text).contain(
+          '<p><button type="submit">Log In</button></p>');
+        
+        done();
+      });
+    });
+  }); // end of get /auth/login 
+
+  describe('POST /auth/login', () => {
+    beforeEach(() => {
+      (async () => { await knex('users').insert({ username: 'test_user', password: '12345'}); }) 
+    });
+
+    it('should show user login form', (done) => {
+      chai.request(server)
+      .post('/auth/login')
+      .send({ username: 'test_user', password: '12345'})
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(res.status).eql(200);
+        expect(res.redirects).contain('/auth/status');
+        
+        done();
+      });
+    });
+  }); // end of post /auth/login 
 })
